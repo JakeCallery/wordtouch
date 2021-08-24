a817f398b126b8612690658db8a6429f8cf973f4
Jake <jake@subvoicestudios.com>
2021-08-24 07:24:00 -0400

    more structure

---
+++ client/src/components/outputBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../styles/outputBar.css';
+
+const OutputBar = (props) => {
+
+    return (
+        <div className={"outputBar"}>
+            <input className={"outputBar"} type="text"/>
+        </div>
+    )
+
+}
+
+export default OutputBar;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import WordButton from "./components/wordButton";
+import OutputBar from "./components/outputBar";
+import './styles/app.css';
 
 const App = () => {
 
     return (
         <div>
             <h1>App</h1>
+            <OutputBar/>
             <WordButton word={"test"}/>
         </div>
     )
