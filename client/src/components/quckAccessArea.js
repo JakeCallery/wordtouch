abb087f9d69e700c4d0840a3ef0406845a6dbec5
Jake <jake@subvoicestudios.com>
2021-08-25 05:58:53 -0400

    minor tag clean up

--- client/src/components/quckAccessArea.js
+++ client/src/components/quckAccessArea.js
@@ -2,7 +2,7 @@ import React from "react";
 import '../styles/quickAccessArea.css';
 const QuickAccessArea = (props) => {
     return(
-        <div className={"quick-access-area"}></div>
+        <div className={"quick-access-area"}/>
     )
 }
 
