f21069abe2de98755c57c67d6d85d5b0e8d000d7
Jake <jake@subvoicestudios.com>
2021-08-26 16:50:34 -0400

    prepping to use an underline style for the cursor location

--- client/src/components/outputBar.js
+++ client/src/components/outputBar.js
@@ -1,12 +1,15 @@
 import React from "react";
 import '../styles/outputBar.css';
 import ClearFieldButton from "./clearFieldButton";
+import ReactHtmlParser from 'react-html-parser';
 
 const OutputBar = ({fieldText, onClearFieldClick}) => {
 
     return (
         <div className={"output-bar"}>
-            <input readOnly type="text" value={fieldText}/>
+            <div className={"output-bar-field"}>
+                { ReactHtmlParser(fieldText)}
+            </div>
             <ClearFieldButton onClearFieldClick={onClearFieldClick}/>
         </div>
     )
