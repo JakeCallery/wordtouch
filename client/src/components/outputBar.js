7766ba9ffc001aeb9e8f16db74c7653dd6428559
Jake <jake@subvoicestudios.com>
2021-08-27 16:35:57 -0400

    auto scrolling output bar to right

--- client/src/components/outputBar.js
+++ client/src/components/outputBar.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import '../styles/outputBar.css';
 import ReactHtmlParser from 'react-html-parser';
 import Backspace from "./backspace";
 
 const OutputBar = ({fieldText, onBackspaceClick}) => {
+    const barRef = useRef();
+
+    useEffect(() => {
+        barRef.current.scrollLeft = barRef.current.scrollWidth;
+    });
+
     fieldText = fieldText + "<span id='cursor'>|</span>";
     return (
         <div className={"output-bar"}>
-            <div className={"output-bar-field"}>
+            <div ref={barRef} className={"output-bar-field"}>
                 { ReactHtmlParser(fieldText) }
             </div>
             <Backspace onBackspaceClick={onBackspaceClick}/>
