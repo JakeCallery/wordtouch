22e5533a700a34642ec052c4aeab5bf45d12cb4a
Jake <jake@subvoicestudios.com>
2021-09-07 08:29:49 -0400

    speech synthesis implemented

--- client/src/components/speechElement.js
+++ client/src/components/speechElement.js
@@ -9,10 +9,38 @@ const SpeechElement = ({wordToSpeak}) => {
         if(wordToSpeak !== "") {
             //TODO: Make call to server to synth word
             console.log("Speaking " + wordToSpeak);
+            speakWord(wordToSpeak);
         }
     },[wordToSpeak])
 
 
+    //http://192.168.2.60:5500/api/tts?voice=larynx%3Acmu_jmk-glow_tts&
+    // vocoder=hifi_gan:vctk_small&
+    // denoiserStrength=0.001&
+    // text=freedom&
+    // cache=true
+    const speakWord = async (word) => {
+        console.log('Fetching...');
+        const res = await fetch(
+            'http://192.168.2.60:5500' +
+            '/api/tts?voice=' + encodeURIComponent('larynx:cmu_jmk-glow_tts') +
+            '&vocoder=' + encodeURIComponent('hifi_gan:vctk_small') +
+            '&denoiserStrength=' + '0.001' +
+            '&text=' + encodeURIComponent(word),
+            {cache: 'no-cache'}
+
+        );
+
+        console.log(res);
+
+        if(res.ok) {
+            const blob = await res.blob();
+            console.log('Speaking...');
+            console.log(audioElement.current);
+            audioElement.current.src = URL.createObjectURL(blob);
+            audioElement.current.play();
+        }
+    };
 
     return (
         <div className={"audio-element"}>
